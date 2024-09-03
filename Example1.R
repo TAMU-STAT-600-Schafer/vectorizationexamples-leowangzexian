a9fba216c5f7f2e1b1ae4e06dffd91c6536adfb6
Leo Zexian Wang <leowang@tamu.edu>
2024-09-03 10:21:10 -0500

    completed Example1.R

--- Example1.R
+++ Example1.R
@@ -2,29 +2,38 @@
 
 square_for <- function(x){
   # [ToDo] Use the for loop
-  square = x^2
-  return(square)
+  # is.numeric(x)
+  # length(x) > 0
+  n = length(x)
+  y = vector(length = n)
+  for (i in 1:n) { # seq_along(x)
+    y[i] = x[i]^2
+  }
+  return(y)
 }
 
 square_sapply <- function(x){
   # [ToDo] Use the sapply function
-
+  sapply(x, \(x){x^2}) # function(x){x^2}
 }
 
 square_vec <- function(x){
   # [ToDo] Use power(^) function in vector form
-
+  x^2
 }
 
 square_vec2 <- function(x){
   # [ToDo] Use multiplication(*) function in vector form
-
+  x*x
 }
 
 # [ToDo] Create a vector x of size 100,000 of normal variables
-
+x = rnorm(100000)
 
 # [ToDo] Verify that all functions return the same output
-
+library(testthat)
+expect_equal(square_vec(x), square_sapply(x))
 
 # [ToDo] Use microbenchmark package to compare three functions in terms of speed
+library(microbenchmark)
+microbenchmark(square_for(x), square_sapply(x), square_vec(x), square_vec2(x), times = 10)
